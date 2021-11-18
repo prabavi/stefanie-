a896451661cb26f192ec90a04ba4b53a35e115e4
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-11-18 07:34:51 +0530

    ✅

--- julie/StringSession.js
+++ julie/StringSession.js
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'JulieMwol:::' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'STEFANIE===' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
